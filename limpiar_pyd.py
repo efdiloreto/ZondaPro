570071c5d3a7283e76e07a6d3ced3c1da4fd9f65
Eduardo Di Loreto <efdiloreto@gmail.com>
2023-04-11 08:51:17 -0400

    Update

--- limpiar_pyd.py
+++
@@ -1,9 +0,0 @@
-import glob
-from pathlib import Path
-
-archivos_pyd = [Path(path) for path in glob.iglob("./zondapro/**/*.pyd", recursive=True)]
-
-
-for archivo in archivos_pyd:
-    print(archivo)
-    archivo.unlink()
