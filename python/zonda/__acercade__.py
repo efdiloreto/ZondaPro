4b2e205785e5abc307067704387e24caf1f41bef
Eduardo Di Loreto <efdiloreto@gmail.com>
2026-09-20 19:25:02 -0400

    Subir la versión a 2.0.3

--- python/zonda/__acercade__.py
+++ python/zonda/__acercade__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Zonda.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 __compania__ = "Eduardo Di Loreto"
 __web_compania__ = "https://github.com/efdiloreto"
 __web__ = "https://github.com/efdiloreto/ZondaPro"
